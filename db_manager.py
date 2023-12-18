eb4d30741360c74edc499899cccd1c49114fdf8b
linurbi <74245540+linurbi@users.noreply.github.com>
2023-12-18 13:13:42 +0200

    Update db_manager.py

--- db_manager.py
+++ db_manager.py
@@ -25,12 +25,3 @@ class DbManager:
     def db_commit(self):
         db = sqlite3.connect(self.db_path)
         db.commit()
-
-# if __name__ == '__main__':
-#     db = DbManager('db.db')
-# #    s = db.execute_query("SELECT * FROM students", return_selection=True)
-#     t = db.execute_query("""SELECT account_balance FROM fact_transactions
-#             WHERE account_number = ? GROUP BY account_number HAVING transaction_date = max(transaction_date) """,
-#                                                     (9962,), False,
-#                                                     False, True)
-#     print(t[0])
